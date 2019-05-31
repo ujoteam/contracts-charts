fa30a94c6c18e68b03ef79b2c78d6b847ebede87
bryn@signals.io <bryn@signals.io>
2019-05-30 20:26:52 -0500

    Add license for RealMath.sol

--- contracts/RealMath.sol
+++ contracts/RealMath.sol
@@ -1,6 +1,31 @@
 pragma solidity 0.5.8;
 
 /**
+ * Taken from https://github.com/NovakDistributed/macroverse/blob/master/contracts/RealMath.sol
+ *
+ * MIT License
+ * Copyright (c) 2017-2018 Novak Distributed
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * ---
+ *
  * RealMath: fixed-point math library, based on fractional and integer parts.
  * Using int128 as real88x40, which isn't in Solidity yet.
  * 40 fractional bits gets us down to 1E-12 precision, while still letting us
